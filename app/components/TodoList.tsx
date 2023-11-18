718a1bc2b73a4fd13a1e35cdccd5af96f6f69b87
Leo Volwahsen <volwahsenl@gmail.com>
2023-11-18 15:46:12 +0100

    Adding a test api to see conecztion working with fetch request

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,9 +1,24 @@
 function TodoList() {
-  return (
-    <div>
-      TodoList
-    </div>
-  )
+    return (
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Favorite Color</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    <tr>
+                        <td>Cy Ganderton</td>
+                        <td>Quality Control Specialist</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default TodoList
