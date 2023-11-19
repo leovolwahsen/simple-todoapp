ed3962fcd40f10bc71768fb969558cc0e3bd1493
Leo Volwahsen <volwahsenl@gmail.com>
2023-11-19 21:10:15 +0100

    error with crud post method adding new task. trying to find out whats the bug

--- api.ts
+++ api.ts
@@ -7,3 +7,26 @@ export const getAllTodos = async (): Promise<ITask[]> => {
     const todos = await res.json();
     return todos;
 }
+
+export const addTodo = async (todo: ITask): Promise<ITask> => {
+    try {
+        const res = await fetch(`${baseUrl}/tasks`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    });
+
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const newTodo = await res.json();
+    console.log('New todo:', newTodo);
+    return newTodo;
+    } catch (error) {
+        console.error('Error adding todo:', error);
+        throw error; 
+    }
+    
+}
